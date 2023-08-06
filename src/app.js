d245f6af5243eaa7e4b3521b19e33e26c68e1a6e
Igor Bandero <igorbandero@gmail.com>
2023-08-06 19:54:49 -0300

    Requisito: modelagem inicial e conexão com o banco de dados implementado

--- src/app.js
+++ src/app.js
@@ -12,4 +12,4 @@ app.use(router);
 dotenv.config();
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => console.log(`Running server on port ${port}`));
